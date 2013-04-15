77fe336273818897beb5c7fd59c42ce1e7f42a9b
Phillip Rhodes <rhodebump@gmail.com>
2013-04-14 21:56:29 -0400

    bug fixes

---
+++ target/classes/com/gwtfb/client/AppImageBundle.java
@@ -0,0 +1,17 @@
+package com.gwtfb.client;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+/**
+ * Application Images
+ * @author ola
+ */
+public interface AppImageBundle extends ClientBundle {
+
+    //this is nice for example, but not for library
+//    @Source ( "logo.png" )
+//    ImageResource logo ();
+    
+
+}
